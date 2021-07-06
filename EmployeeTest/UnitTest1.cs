ee6361a2de50ee414837efc9f60c7404c3099f4d
Nick <nick@nixon.me>
2021-07-06 14:22:55 +0300

    Implemented test cases

--- EmployeeTest/UnitTest1.cs
+++ EmployeeTest/UnitTest1.cs
@@ -1,4 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Employee;
+using System;
+using System.IO;
 
 namespace EmployeeTest
 {
@@ -6,8 +9,46 @@ namespace EmployeeTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEmployees()
         {
+            var lines = File.ReadAllLines("./test.txt");
+            Employees employees = new Employees(lines);
+            Assert.AreEqual(3300,employees.SalaryBudget("Employee1"));
+            Assert.AreEqual(1000,employees.SalaryBudget("Employee2"));
+ 
+        }
+        //test invalid salary value for employee 6
+        [FactMethod]
+        public void Test3()
+        {
+            var lines = File.ReadAllLines("./test3.txt");
+            Employees employees = new Employees(lines);
+            Assert.AreEqual(3300,employees.SalaryBudget("Employee1"));
+            Assert.AreEqual(1300,employees.SalaryBudget("Employee2"));
+            Assert.AreEqual(500,employees.SalaryBudget("Employee3"));
+            Assert.AreEqual(0,employees.SalaryBudget("Employee6"));
+ 
+        }
+
+        [FactMethod]
+        public void Test2()
+        {
+            var lines = File.ReadAllLines("./test2.txt");
+            Employees employees = new Employees(lines);
+            Assert.AreEqual(3800,employees.SalaryBudget("Employee1"));
+            Assert.AreEqual(1800,employees.SalaryBudget("Employee2"));
+            Assert.AreEqual(500,employees.SalaryBudget("Employee3"));
+ 
+        }
+
+         [FactMethod]
+        public void TestDoubleLink()
+        {
+            var lines = File.ReadAllLines("./../test1.txt");
+            Employees employees = new Employees(lines);
+            Assert.AreEqual(3300,employees.SalaryBudget("Employee1"));
+            Assert.AreEqual(1000,employees.SalaryBudget("Employee2"));
+ 
         }
     }
 }
